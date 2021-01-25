e31ce3e2e2440e47e8974f22969dfa21d2acc17a
Felipe Romero <feliperomero3@outlook.com>
2021-01-24 23:09:53 -0600

    Prepare a Database.

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly EFDbContext _context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return _context.Products;
+            }
+        }
+    }
+}
--- SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
-using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
-using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -30,15 +29,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            var mock = new Mock<IProductRepository>();
-
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
